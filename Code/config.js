2e536c26beca8b49cef3bfd7e5e207d90714ca1f
Parth Garg <parth.garg2024@vitstudent.ac.in>
2026-01-13 00:54:23 +0530

    Add supabase key

--- Code/config.js
+++ Code/config.js
@@ -3,7 +3,7 @@
 const supabaseUrl = "https://xovxxhnsezfnuioamhxu.supabase.co";
 
 // TODO: Paste your 'anon' key here (It starts with "eyJ...")
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY_HERE";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvdnh4aG5zZXpmbnVpb2FtaHh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyMzk5MjEsImV4cCI6MjA4MzgxNTkyMX0.wktsf8krEL9b-X0uh4Ge64Nb5upUM3uAawKCcpkpueg";
 
 // Make available to the app
 window.supabaseUrl = supabaseUrl;
